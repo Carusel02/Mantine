b43c10586259a5e1b3fa1c8a5d432dfe8c46a0f1
Diana <dianastefan.867@gmail.com>
2024-11-18 13:22:39 +0200

    remove unnecessary pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,18 @@
-import {Stack, Button, Flex, Title, Container} from '@mantine/core'
-import Link from "next/link";
+'use client';
 
-export default function HomePage() {
-  return (
+import Header from './startup-page/Header';
+import Body from './startup-page/Body';
+import Footer from './startup-page/Footer';
+import { Stack } from '@mantine/core';
 
-      <Stack align="center" justify="center" gap="md" margins="md">
-
-          <Title order={1}> Welcome to HomeHunters! </Title>
-
-
-          <Title order={2}> Have an account? </Title>
-
-
-          <Link href="/signin" passHref>
-              <Button
-                variant="filled"
-                color="gray"
-                radius="md"
-
-                align="center"
-              >
-                Sign In
-              </Button>
-          </Link>
-
-          <Link href="/startup-page" passHref>
-              <Button
-                  variant="filled"
-                  color="gray"
-                  radius="md"
-
-                  align="center"
-              >
-                  Startup Page
-              </Button>
-          </Link>
-
-
-
-
-
-          <Title order={2}> Don't have an account? </Title>
-
-
-            <Button
-                variant="filled"
-                color="gray"
-                radius="md"
-                component="a"
-                href="/signup"
-            >
-                Sign Up
-            </Button>
-
-
-
-          <Title order={2}> Details </Title>
-
-              <Button
-                  variant="filled"
-                  color="gray"
-                  radius="md"
-                  component="a"
-                  href="/another-page"
-              >
-                  Go to another page
-              </Button>
-
-
-              <Button
-                  variant="filled"
-                  color="gray"
-                  radius="md"
-                  component="a"
-                  href="/portfolio-page"
-              >
-                  Go to Portfolio
-              </Button>
-
-      </Stack>
-
-  );
+export default function StartupPage() {
+    return (
+        <div>
+            <Stack>
+                <Header />
+                <Body   />
+                <Footer />
+            </Stack>
+        </div>
+    );
 }
