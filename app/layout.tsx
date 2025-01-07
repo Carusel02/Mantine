4be2ddfcc68809d8ef6375f74fb3a5ac62e45787
Marius Daniel <marinmariusdaniel02@gmail.com>
2025-01-08 00:03:59 +0200

    switch icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import {theme} from "../theme";
 import {AuthContextProvider} from "./context/AuthContext";
 
 export const metadata = {
-    title: "Mantine Next.js template",
-    description: "I am using Mantine with Next.js!",
+    title: "HomeHunters",
+    description: "Best real estate app!",
 };
 
 
@@ -16,7 +16,7 @@ export default function RootLayout({children}: { children: any }) {
         <html lang="en" suppressHydrationWarning>
         <head>
             <ColorSchemeScript/>
-            <link rel="shortcut icon" href="/favicon.svg"/>
+            {/* <link rel="shortcut icon" href="/favicon.svg"/> */}
             <meta
                 name="viewport"
                 content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
